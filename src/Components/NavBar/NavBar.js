610c9524bb4626d3a5db4bd13f0feebe98497055
Nicolas Cuchero <n.cuchero@gmail.com>
2021-10-02 17:37:30 -0300

    Agrego NavBar y Navbar.css

---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './NavBar.css'
+
+const NavBar = () => {
+    return (
+        <header className = "header">
+            <h1>UShop</h1>
+            <div>
+                <nav>
+                    <p>Enlace 1</p>
+                    <p>Enlace 2</p>
+                    <p>Enlace 3</p>
+                </nav>
+            </div>
+        </header>
+    )
+}
+
+export default NavBar
+
